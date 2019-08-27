b5e5e1724579078de57ca9e1971c2d4bc66bd4e2
Giovanni <giovannigiampaolo58@gmail.com>
2019-08-27 10:02:07 +0200

    Updates

---
+++ precache-manifest.4f69cd9e3cb5b3c0c35325cf02a6b471.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ec26e1e4292e0df61ac067a769fd1b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7c007f132ea6b203350b",
+    "url": "/monsters-rolodex/static/css/main.79d70424.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7c007f132ea6b203350b",
+    "url": "/monsters-rolodex/static/js/main.6635290d.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
